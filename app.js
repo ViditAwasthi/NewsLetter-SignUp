adf0221195c555b27e81b15dd30c2ad39943c532
Vidit Awasthi <viditawasthi2@gmail.com>
2020-06-01 01:47:32 +0530

    First one

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https")
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.get("/", function(req, res){
+  app.use(express.static("public"));
+
+res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req, res){
+
+const firstName =req.body.fName;
+const lastName =req.body.lName;
+const email =req.body.email;
+
+const data = {
+
+  members : [
+    {
+      email_address: email,
+      status : "subscribed",
+      merge_fields:{
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }
+  ]
+};
+
+const jsonData = JSON.stringify(data);
+const url = "https://us10.api.mailchimp.com/3.0/lists/6a00d54a94"
+
+const options = {
+  method : "POST",
+  auth: "vidit:0c55b455a9fc2306d0952b4e7d2b447f-us10"
+}
+const request = https.request(url, options, function(response){
+  response.on("data",function(data){
+    console.log(JSON.parse(data));
+  })
+
+})
+request.write(jsonData);
+request.end();
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+
+  console.log("Server Started at 3000");
+})
+
+
+// 0c55b455a9fc2306d0952b4e7d2b447f-us10
